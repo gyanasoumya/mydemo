310e01ab2878a8a9dd2ac71063da473cde8bb996
gyanasoumya <32173723+gyanasoumya@users.noreply.github.com>
2017-10-16 14:01:59 +0530

    Update DataReadCol5.java

--- DataReadCol5.java
+++ DataReadCol5.java
@@ -8,15 +8,17 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import java.io.FileInputStream;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
-public class DataReadCol4 {
+public class DataReadCol7 {
 
 	public static void main(String[] args) throws Exception {
 
@@ -26,7 +28,7 @@ public class DataReadCol4 {
 			String json = "{\"id\":" + CustomerConstant.id + ",\"lid\":" + CustomerConstant.lid + ",\"association\":"
 					+ CustomerConstant.association + ",\"classificationNodes\":[";
 
-			InputStream inp = new FileInputStream("F:/Gyana_Data/country.xlsx");
+			InputStream inp = new FileInputStream("D:/eclipse-mars-workspace/XLReader/input/country2.xlsx");
 			int ctr = 0;
 			Workbook wb = WorkbookFactory.create(inp);
 			Sheet sheet = wb.getSheetAt(0);
@@ -34,59 +36,57 @@ public class DataReadCol4 {
 			Iterator rowIter = (sheet).iterator();
 			int rownum = 1;
 
-
-			Set countries = new HashSet<Map>();
-			Map<String, Set> countryMap = new HashMap<String, Set>();
+			Map<String, Map<String, Set<String>>> countryMap = new HashMap<String, Map<String, Set<String>>>();
 
 			while (rowIter.hasNext()) {
+				Account ac = new Account();
 				Row myRow = (Row) rowIter.next();
 				Iterator cellIter = myRow.cellIterator();
 				if (rownum > 1) {
 					Vector<String> cellStoreVector = new Vector<String>();
-					Set<Account> unicRow = new HashSet<Account>();
 
-					Map<String, Set> accountMap = new HashMap<String, Set>();
 					// --------------------------
 					String country = null;
 					String account = null;
 					String currency = null;
 
-					Set<Map> accountSet = null;
-					Set currencySet = null;
+					Map<String, Set<String>> accountMap = null;
+					Set<String> currencys = null;
 
 					// --------------------------
-
 					int columnIdex = 0;
 					while (cellIter.hasNext()) {
 
 						Cell myCell = (Cell) cellIter.next();
-						System.out.print(myCell.toString());
-						System.out.print(" ");
+						//System.out.print(myCell.toString());
+						//System.out.print(" ");
 						String cellvalue = myCell.toString();
+
 						if (columnIdex == 0) {
 
 							country = cellvalue;
-							accountSet = countryMap.get(country);
-							if (accountSet == null) {
-								accountSet = new HashSet<Map>();
-								//countryMap.put(country, accountSet);
+							accountMap = countryMap.get(country);
+							if (accountMap == null) {
+								// sortedAccounts = new ArrayList<Account>();
+								accountMap = new HashMap<String, Set<String>>();
 							}
 
 						} else if (columnIdex == 1) {
 							account = cellvalue;
-							currencySet = accountMap.get(account);
-							if (currencySet == null) {
-								currencySet = new HashSet<>();
-								//accountMap.put(account, currencySet);
+							currencys = accountMap.get(account);
+							if (currencys == null) {
+								currencys = new HashSet<String>();
 							}
+
+							ac.setAccountType(account);
 
 						} else if (columnIdex == 2) {
 							currency = cellvalue;
-							currencySet.add(currency);
-							accountMap.put(account, currencySet);
-							accountSet.add(accountMap);
-							countryMap.put(country, accountSet);
-							//countries.add(countryMap);
+							currencys.add(currency);
+							accountMap.put(account, currencys);
+							//System.out.println(accountMap);
+							countryMap.put(country, accountMap);
+							System.out.println(countryMap);
 						}
 
 						cellStoreVector.addElement(cellvalue);
@@ -108,18 +108,18 @@ public class DataReadCol4 {
 			// iterate country from countryMap object
 
 			Set<String> countryKeys = countryMap.keySet();
-			System.out.println("-----------------------------------");
+			System.out.println("---------------------------------------------------------------------------");
 			for (String country : countryKeys) {
 				// System.out.println(country);
 
-				Set<Map> accountSet = countryMap.get(country);
-				for (Map<String, Set<String>> accountMap : accountSet) {
-					Set<String> accountKeys = accountMap.keySet();
-					for (String account : accountKeys) {
-						Set<String> currencySet = accountMap.get(account);
-						for (String currency : currencySet) {
-							System.out.println(country + "   " + account + "   " + currency);
-						}
+				Map<String, Set<String>> accountMap = countryMap.get(country);
+				// Collections.sort(accountMap);
+				System.out.println();
+				Set<String> accounts = accountMap.keySet();
+				for (String account : accounts) {
+					Set<String> currencys = accountMap.get(account);
+					for (String currency : currencys) {
+						System.out.println(country + "  " + account + "  " + currency);
 					}
 				}
 
@@ -150,26 +150,19 @@ public class DataReadCol4 {
 			// json = json + "]}";
 			// System.out.println(json);
 		} catch (IOException e) {
-
 			e.printStackTrace();
-
 		} finally {
-
 			if (fis != null) {
-
 				fis.close();
-
 			}
-
 		}
 
 		// showExelData(sheetData);
-
 	}
 
 	private static void insertQuery(String firstcolumnvalue, String secondcolumnvalue) {
 
-		System.out.println(firstcolumnvalue + " " + secondcolumnvalue);
+		//System.out.println(firstcolumnvalue + " " + secondcolumnvalue);
 
 	}
 
